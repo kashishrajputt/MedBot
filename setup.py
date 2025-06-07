3a5b2af5d9029ca74bee850a96cc05a346997671
kashishrajputt <btecg41522029@dseu.ac.in>
2025-06-07 17:42:05 +0530

    folder structure edit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="medbot",  # no spaces or special characters
+    version="0.1.0",  # valid version format
+    packages=find_packages(),
+    install_requires=[
+        # your dependencies
+    ],
+)
